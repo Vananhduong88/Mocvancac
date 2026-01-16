7c5115ba84cae5d37b412e0411d59c3300b94b4e
Vananhduong88 <anhdtv.tdh@gmail.com>
2026-01-16 18:41:36 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// DARK MODE
 document.getElementById("darkToggle").onclick = () => {
   document.body.classList.toggle("dark");
 };
-
-// SLIDER
-const slides = document.querySelector(".slides");
-let index = 0;
-
-setInterval(() => {
-  index++;
-  if (index > 4) index = 0;
-  slides.style.transform = `translateX(-${index * 235}px)`;
-}, 3000);
