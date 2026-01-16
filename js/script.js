a95abe162a7c7b2f791f35f3171a840e429c1354
Vananhduong88 <anhdtv.tdh@gmail.com>
2026-01-16 19:10:34 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// Dark mode
-document.getElementById("darkToggle").onclick = () => {
+const toggle = document.getElementById("darkToggle");
+toggle.onclick = () => {
   document.body.classList.toggle("dark");
 };
-
-const slider = document.getElementById("hotSlider");
-let x = 0;
-
-setInterval(() => {
-    x += 260;
-    if (x >= slider.scrollWidth - slider.clientWidth) x = 0;
-    slider.scrollTo({ left: x, behavior: "smooth" });
-}, 2500);
